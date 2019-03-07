38db670bd593be958f597f82693a7b6156ca5bc4
bar0191 <brandonreid2@my.unt.edu>
2019-03-07 09:23:40 -0500

    added weather service controllers

---
+++ src/server/services/weather.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+
+const Weather = () => {};
+
+const apiRoute = 'https://www.metaweather.com/api/location/';
+
+Weather.searchByLatLong = (query, callback) => {
+  fetch(`${apiRoute}search/?lattlong=${query}`)
+    .then(res => res.json())
+    .then(data => callback(null, data))
+    .catch(err => callback(err, null));
+};
+
+Weather.searchByCity = (query, callback) => {
+  fetch(`${apiRoute}search/?city=${query}`)
+    .then(res => res.json())
+    .then(data => callback(null, data))
+    .catch(err => callback(err, null));
+};
+
+Weather.getLocation = (query, callback) => {
+  fetch(`${apiRoute}${query}`)
+    .then(res => res.json())
+    .then(data => callback(null, data))
+    .catch(err => callback(err, null));
+};
+
+
+module.exports = Weather;
